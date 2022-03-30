bbbf3a0f620b21e746d944d88a47d1b9ed0ce21e
dragonflylee <dragonflylee@outlook.com>
2022-03-30 13:26:16 +0800

    禁用 Windows Defender

--- Pack/Scripts/SetupComplete.cmd
+++ Pack/Scripts/SetupComplete.cmd
@@ -1,4 +1,12 @@
 @echo off
+
+rem Disable Tasks
+schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable
+
 powercfg /h off
 "%~dp0AAct.exe" /taskwin /wingvlk /win=act
 attrib -r -a -s -h "%~dp0\*.*"
